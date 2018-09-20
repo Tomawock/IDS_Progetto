3fcb7630c540b51ceea1089ed2b09cf92f196fa1
Tomawock <lori.tomasetti@hotmail.it>
2018-09-20 18:18:43 +0200

    fix

--- src/versione_2/model/Libro.java
+++ src/versione_2/model/Libro.java
@@ -1,5 +1,32 @@
 package versione_2.model;
 
-public class Libro {
+import java.util.ArrayList;
+
+public class Libro  implements Risorsa{
+	
+	private int n_licenze;
+	private String id_libro;
+	//private id_inivoco= id_libro+n_licenza
+	
+
+	private String titolo; 
+	private String autore;
+	private String numero_di_pagine; 
+	private String anno_di_pubblicazione; 
+	private String casa_editrice;
+	private String lingua;
+	private String genere;
+
+	@Override
+	public void aggiungi_descrizione(ArrayList<String> dati) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void rimuovi_descrizione() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
