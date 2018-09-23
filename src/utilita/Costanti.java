685256400bd963ea1bad9d9d11f633ae2f7388c3
Tomawock <lori.tomasetti@hotmail.it>
2018-09-23 11:58:02 +0200

    Inserimento Prestito

--- src/utilita/Costanti.java
+++ src/utilita/Costanti.java
@@ -1,11 +1,16 @@
 package utilita;
 
+import java.time.temporal.TemporalAmount;
+
 public class Costanti {
 
 	public final static String NO_DESCRIZIONE="Descrizione non inserita";
-	public final static int SCADENZA_TERMINE_FRUITORE = 5;
-	public final static int GIORNI_RINNOVO_ISCRIZIONE = 10;
+	public final static long SCADENZA_TERMINE_FRUITORE = 5;
+	public final static long GIORNI_RINNOVO_ISCRIZIONE = 10;
 	public final static String GRECA="****************************************************************";
 	public final static String GRECA2 = "================================================================";
 	public final static String GRECA3 = "_______________________________________________________________";
+	public static final long DURATA_PRESTITO = 30;
+	public static final long RICHIESTA_PROROGA = 3;
+	public static final long DURATA_PROROGA = 15;
 }
---
+++ src/versione_3/model/Prestito.java
@@ -0,0 +1,65 @@
+package versione_3.model;
+
+import java.time.LocalDateTime;
+
+import utilita.Costanti;
+
+public class Prestito {
+	
+	private Risorsa risorsa;
+	private Fruitore fruitore;
+	
+	private LocalDateTime data_inizio_prestito;
+	private LocalDateTime data_fine_prestito;
+	private LocalDateTime data_proroga_prestito;
+	
+	private LocalDateTime data_inizio_proroga;
+	
+	private boolean mai_progato;
+
+	public Prestito(Risorsa risorsa, Fruitore fruitore) {
+		this.risorsa = risorsa;
+		this.fruitore = fruitore;
+		this.data_inizio_prestito=LocalDateTime.now();
+		this.data_fine_prestito=this.data_inizio_prestito.plusDays(Costanti.DURATA_PRESTITO);
+		this.data_proroga_prestito=this.data_fine_prestito.minusDays(Costanti.RICHIESTA_PROROGA);
+		this.mai_progato=true;
+	}
+
+	public Risorsa getRisorsa() {
+		return risorsa;
+	}
+
+	public Fruitore getFruitore() {
+		return fruitore;
+	}
+
+	public LocalDateTime getData_inizio_prestito() {
+		return data_inizio_prestito;
+	}
+
+	public LocalDateTime getData_fine_prestito() {
+		return data_fine_prestito;
+	}
+
+	public LocalDateTime getData_proroga_prestito() {
+		return data_proroga_prestito;
+	}
+
+	public boolean isMai_progato() {
+		return mai_progato;
+	}
+	
+	public boolean is_rinnovabile() {
+		if(LocalDateTime.now().isAfter(data_proroga_prestito) && this.mai_progato) {
+			return true;
+		}
+		return false;
+	}
+	
+	public void rinnova() {
+		this.mai_progato=false;
+		this.data_inizio_proroga=LocalDateTime.now();
+		this.data_fine_prestito=this.data_inizio_proroga.plusDays(Costanti.DURATA_PROROGA);
+	}
+}
