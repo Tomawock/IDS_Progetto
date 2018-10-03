13db5378c577d7bff110d134828311007f4bcfdf
Tomawock <lori.tomasetti@hotmail.it>
2018-10-03 10:34:57 +0200

    aggiunti commenti v_1 controller

--- src/versione_1/controller/Controller.java
+++ src/versione_1/controller/Controller.java
@@ -15,11 +15,14 @@ public class Controller {
 		view=new View();
 		db=new Database_file();
 	}
-		
+	
+	/**
+	 * Funzione principale che consente di accedere come utente o di registarrsi o di terminare l'esecuzione del programma	
+	 */
 	public void log_in(){
 		int valore=view.log_in_scelta();
 		db.aggiorna_validita_fruitori();//elimina eventuali fruitori non validi
-		if (valore== 1) {
+		if (valore== 1) {//Effetto il log in come utente
 			String utente=view.log();
 			String utente_user=utente.split(IO.SEPARATORE_STRINGHE)[0];
 			String utente_psw=utente.split(IO.SEPARATORE_STRINGHE)[1];
@@ -32,7 +35,7 @@ public class Controller {
 				this.user_loggato(db.carica_utente(utente_user, utente_psw));
 			}
 		}
-		else if(valore == 2) {
+		else if(valore == 2) {//Registro un nuovo utente
 			ArrayList<String> nuovo_utente=view.nuova_registrazione();
 			Utente user= new Utente(nuovo_utente);
 			if(!db.is_presente(user)) {
@@ -46,16 +49,18 @@ public class Controller {
 			//una volta completata l'iscrizione torna al log in
 			this.log_in();
 		}
-		else if (valore ==3) {
+		else if (valore ==3) {//Caso di uscita 
 			return;
 		}
 	}
 
-	
-	
+	/**
+	 * Parte che gestisce il post log in dato un Utente
+	 * @param utente	utente che ha effettuato il log in
+	 */
 	public void user_loggato(Utente utente){
 		int valore=view.log_fruitore_operatore(utente);
-		if (valore== 1) {
+		if (valore== 1) {//Loggare come Fruitore
 			Fruitore fruitore=db.carica_fruitore(utente.getUsername(), utente.getPassword());
 			if(fruitore==null) {
 				view.scrivi("Non sei registrato come fruitore");
@@ -70,7 +75,7 @@ public class Controller {
 			this.fruitore_loggato(fruitore);
 			}
 		}
-		else if(valore == 2) {
+		else if(valore == 2) {//Loggare come Operatore
 			Operatore operatore=db.carica_operatore(utente.getUsername(), utente.getPassword());
 			if(operatore==null) {
 				view.scrivi("Non sei registrato come operatore");
@@ -80,7 +85,7 @@ public class Controller {
 				this.operatore_loggato(operatore);
 			}
 		}
-		else if(valore==3) {
+		else if(valore==3) {//Registra nuovo Fruitore
 			if(db.carica_fruitore(utente.getUsername(), utente.getPassword())!=null) {
 				view.scrivi("Sei gia registrato come fruitore");
 				this.user_loggato(utente);
@@ -96,7 +101,7 @@ public class Controller {
 				this.user_loggato(utente);
 			}
 		}
-		else if(valore==4) {
+		else if(valore==4) {//Registra nuovo Operatore
 			if(db.carica_operatore(utente.getUsername(), utente.getPassword())!=null) {
 				view.scrivi("Sei gia registrato come operatore");
 				this.user_loggato(utente);
@@ -108,7 +113,7 @@ public class Controller {
 				this.user_loggato(utente);
 			}
 		}
-		else if(valore==5) {
+		else if(valore==5) {//Torna alla pagina di log in
 			this.log_in();
 		}
 		else {
@@ -117,29 +122,37 @@ public class Controller {
 		}
 	}
 
+	/**
+	 * Gestione delle opzioni dell'Operatore
+	 * @param operatore	operatore selezionato sul quale vengono svolti i casi d'uso
+	 */
 	private void operatore_loggato(Operatore operatore) {
 		int scelta = view.operatore_view(operatore);
-		if (scelta==1) {
+		if (scelta==1) {//Visualizza tutti i fruitori presenti
 			this.view.stampa_fruitori(db.carica_tutti_fruitori());
 			this.operatore_loggato(operatore);
 		}
-		else if (scelta==2){
+		else if (scelta==2){//Torna indietro
 			this.user_loggato(operatore.getUtente());
 		}
 		else {
 			this.user_loggato(operatore.getUtente());
 		}		
 	}
-
+	
+	/**
+	 * Gestione delle opzioni di Fruitore
+	 * @param fruitore Fruitore selezionato sul quale vengono svolti i casi d'uso
+	 */
 	private void fruitore_loggato(Fruitore fruitore) {
 		if(fruitore.is_rinnovabile()) {
 			this.view.scrivi("Scadenza rinnovo vicina ti mancano " + fruitore.get_giorni_scadenza() + " giorni alla scadenza");
 		}
 		int scelta=view.fruitore_view(fruitore);
-		if(scelta ==1) {
+		if(scelta ==1) {//Torna indietro
 			this.user_loggato(fruitore.getUtente());
 		}
-		else if(scelta ==2) {
+		else if(scelta ==2) {//Rinnovo del fruitore
 			if(fruitore.is_rinnovabile()) {
 				db.elimina_fruitore(fruitore);
 				fruitore.rinnova_iscrizione();//rinnova oggetto ma non il db
