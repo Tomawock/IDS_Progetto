15bdee7975e6c847f828b57dfaf1fecdc898beb7
Tomawock <lori.tomasetti@hotmail.it>
2018-10-03 16:31:50 +0200

    inizio v_5

--- src/versione_5/model/Query.java
+++ src/versione_5/model/Query.java
@@ -1,6 +1,8 @@
 package versione_5.model;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
 public class Query {
@@ -23,8 +25,15 @@ public class Query {
 	 * @return	il numero di prestiti effettuati in un anno solare 
 	 */
 	 public int count_numero_di_prestiti_per_anno_solare(LocalDateTime anno) {
-		 db.carica_tutti_prestiti();//sono tutti i prestiti ?
-		return 0;
+		 ArrayList<Prestito> prestiti_archiviati=db.carica_tutti_prestiti();//sono tutti i prestiti ? perchè questi sarebbero quelli "locali e non dell'archivio COME DIO CANE LA FAMO STA COSA"
+		 int res=0;
+		 for(Prestito p:prestiti_archiviati) {
+			 if(p.getData_inizio_prestito().getYear()==anno.getYear()) {
+				 res++;
+			 }
+		 }
+		 
+		return res;
 		 
 	 }
 	 
@@ -35,8 +44,18 @@ public class Query {
 	  * @return	il numero di proroghe effettaute in un anno solare
 	  */
 	 public int count_numero_di_proroghe_per_anno_solare(LocalDateTime anno) {
-		return 0;
+		 ArrayList<Prestito> prestiti_archiviati=db.carica_tutti_prestiti();//sono tutti i prestiti ? perchè questi sarebbero quelli "locali e non dell'archivio COME DIO CANE LA FAMO STA COSA"
+		 int res=0;
+		 for(Prestito p:prestiti_archiviati) {
+			 if(p.getData_inizio_proroga()!=null) {
+				 if(p.getData_inizio_proroga().getYear()==anno.getYear()) {
+					 res++;
+				 }
+				 
+			 }
+		 }
 		 
+		return res; 
 	 }
 	 
 	 /**
@@ -48,7 +67,24 @@ public class Query {
 	  *			//TODO una o piu??
 	  */
 	 public Risorsa select_risorsa_con_max_numero_prestiti(LocalDateTime anno) {
-		return null;
+		ArrayList<Prestito> prestiti_archiviati=db.carica_tutti_prestiti();
+		HashMap<Risorsa, Integer> risultato= new HashMap<Risorsa, Integer>();
+		 for(Prestito p:prestiti_archiviati) {//prende tutti i prestiti
+			Risorsa risorsa=p.getRisorsa();//seleziona unarisrsa
+			Integer num_ripetizioni_risorsa=0;//valore del conteggio delle ripetizioni della risorsa
+			if(!risultato.containsKey(risorsa)) {//se non contiene la chiave allora è una nuova risorsa altrimenti calcolo gia fatto
+				risultato.put(risorsa, num_ripetizioni_risorsa);
+				for(Prestito p1:prestiti_archiviati){
+					if(p1.getRisorsa().equals(risorsa)) {
+						num_ripetizioni_risorsa++;
+					}
+				}
+				risultato.put(risorsa, num_ripetizioni_risorsa);//aggiorna l'hash map con il valore delle ripetizioni corretto
+			}
+		 }
+		 //ricerco il massimo nelle coppie risorsa ripetiziine
+		 
+		return risultato;
 		 
 	 }
 	 
@@ -58,7 +94,7 @@ public class Query {
 	  * @param anno solare dentro il cui cercare
 	  * @return un'associazione fra un Fruitore e il numero di prestiti ad esso relativo
 	  */
-	 public Map<Fruitore, Integer> select_count_numero_di_prestiti_perogni_fruitore(LocalDateTime anno) {
+	 public HashMap<Fruitore, Integer> select_count_numero_di_prestiti_perogni_fruitore(LocalDateTime anno) {
 		return null;
 		 
 	 }
