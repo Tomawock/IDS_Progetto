63e3737d642032fe4659567f9aad011aaf6d9979
Tomawock <lori.tomasetti@hotmail.it>
2019-02-17 11:49:31 +0100

    aggiunto primo test di classe per operatore

---
+++ src/versione_5/test/Operatore_test.java
@@ -0,0 +1,61 @@
+package versione_5.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.Test;
+
+import versione_5.model.*;
+
+class Operatore_test {
+	
+	@Test
+	void true_if_two_operatori_are_the_same() {
+		Utente u_pass= new Utente("test", 
+									"test_c", 
+									"mail",
+									LocalDateTime.now(),
+									"12",
+									"test",
+									"123");
+			
+		Utente u_pass2= new Utente("test", 
+									"test_c", 
+									"mail",
+									LocalDateTime.now(),
+									"12",
+									"test",
+									"123");
+		
+		Operatore o_pass=new Operatore(u_pass);
+		Operatore o_pass2=new Operatore(u_pass2);
+		
+		assertTrue(o_pass.equals(o_pass2),"I due operatori con lo stesso username sono uguai");
+		
+
+	}
+	@Test
+	void true_if_two_operatori_are_not_the_same() {
+		Utente u_pass= new Utente("test", 
+									"test_c", 
+									"mail",
+									LocalDateTime.now(),
+									"12",
+									"test",
+									"123");
+			
+		Utente u_fail= new Utente("test", 
+									"test_c", 
+									"mail",
+									LocalDateTime.now(),
+									"12",
+									"testt",
+									"123");
+		
+		Operatore o_pass=new Operatore(u_pass);
+		Operatore o_fail=new Operatore(u_fail);
+		
+		assertFalse(o_pass.equals(o_fail),"Operatori con diverso username sono diversi");
+	}
+}
