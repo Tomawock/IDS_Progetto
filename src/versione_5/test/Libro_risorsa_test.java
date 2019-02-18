f291702b6d5c79f2d4ef1d7f04fea3f5a3d03a33
Tomawock <lori.tomasetti@hotmail.it>
2019-02-18 15:06:38 +0100

    aggiunte le calssi di test per libro e film che consento la stesura dei test per la classe astratta Risorsa

---
+++ src/versione_5/test/Libro_risorsa_test.java
@@ -0,0 +1,16 @@
+package versione_5.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import versione_5.model.*;
+
+class Libro_risorsa_test {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
