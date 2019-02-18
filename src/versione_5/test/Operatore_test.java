fdf91ff1c840ac0d7f321dacc76c45bd324a6b60
Tomawock <lori.tomasetti@hotmail.it>
2019-02-18 09:15:30 +0100

    aggiunti JunitTest per classe Operatore

--- src/versione_5/test/Operatore_test.java
+++ src/versione_5/test/Operatore_test.java
@@ -9,15 +9,17 @@ import org.junit.jupiter.api.Test;
 import versione_5.model.*;
 
 class Operatore_test {
-	
+	/**
+	 * Test Per cammini di base
+	 */
 	@Test
-	void true_if_two_operatori_are_the_same() {
+	public void true_if_two_operatori_are_the_same() {
 		Utente u_pass= new Utente("test", 
 									"test_c", 
 									"mail",
 									LocalDateTime.now(),
 									"12",
-									"test",
+									"Username",
 									"123");
 			
 		Utente u_pass2= new Utente("test", 
@@ -25,7 +27,7 @@ class Operatore_test {
 									"mail",
 									LocalDateTime.now(),
 									"12",
-									"test",
+									"Username",
 									"123");
 		
 		Operatore o_pass=new Operatore(u_pass);
@@ -35,14 +37,15 @@ class Operatore_test {
 		
 
 	}
+	
 	@Test
-	void true_if_two_operatori_are_not_the_same() {
+	public void true_if_two_operatori_are_not_the_same() {
 		Utente u_pass= new Utente("test", 
 									"test_c", 
 									"mail",
 									LocalDateTime.now(),
 									"12",
-									"test",
+									"Username",
 									"123");
 			
 		Utente u_fail= new Utente("test", 
@@ -50,12 +53,24 @@ class Operatore_test {
 									"mail",
 									LocalDateTime.now(),
 									"12",
-									"testt",
+									"Username_diverso",
 									"123");
 		
 		Operatore o_pass=new Operatore(u_pass);
 		Operatore o_fail=new Operatore(u_fail);
 		
 		assertFalse(o_pass.equals(o_fail),"Operatori con diverso username sono diversi");
+	}
+	
+	/**
+	 * Test boundary value analysis
+	 */
+	@Test
+	public void execute_equals_on_null_constuctor_should_throw_NullPointerException() {
+		Operatore op = new Operatore(null);
+		assertThrows(NullPointerException.class,
+	            ()->{
+	            op.equals(op);
+            	});
 	}
 }
